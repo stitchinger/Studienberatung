e539f721bf9ec7f05188f58c9a086e91da15a95a
Stitchinger <gs.stitch@gmail.com>
2015-11-04 12:59:55 +0100

    Scanner implementiert

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
+
 
 public class Studienberatung {
-
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String answ = scanner.next();
+		
+	}
 }
